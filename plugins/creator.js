80e83e8636504d3fe7b71d5a60a2d895bb0afcbb
Kaguya Shinomiya <102699105+RyhnXD@users.noreply.github.com>
2022-08-11 05:48:32 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,9 +1,16 @@
-function handler(m) {
-  let data = global.owner.filter(([id, isCreator]) => id && isCreator)
-  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
+let handler = async (m, { conn }) => {
+/*
+  const data = global.owner.filter(([id, isCreator]) => id && isCreator)
+  const town = await conn.sendContact(m.chat, data.map(([id, name]) => [id, name]), m, {quoted: fkontak})
+  await conn.reply(m.chat, "My Owner", m, {quoted: town})
+  */
+  let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;${author};;;\nFN:${author}\nORG:${author}\nTITLE:\nitem1.TEL;waid=62895627514070:+62 895-2420-8209\nitem1.X-ABLabel:${author}\nX-WA-BIZ-DESCRIPTION:${htjava} Jangan di spam!\nX-WA-BIZ-NAME:${author}\nEND:VCARD`
+await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard }] }}, {quoted: m})
+
 }
-handler.alias = ['owner']
+handler.help = ['owner', 'creator']
 handler.tags = ['info']
-handler.command = /^(owner)$/i
+
+handler.command = /^(owner|creator)$/i
 
 export default handler
