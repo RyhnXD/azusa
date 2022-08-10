cfb8ac0f5fb83420c0b2f6289130b950d367edf7
Kaguya Shinomiya <102699105+RyhnXD@users.noreply.github.com>
2022-08-10 17:29:36 +0700

    Update menu2.js

--- plugins/menu2.js
+++ plugins/menu2.js
@@ -2,7 +2,7 @@ import moment from 'moment-timezone'
 let handler = async (m, { conn, usedPrefix, __dirname, text, command }) => {
 let date = moment.tz('Asia/Jakarta').format("dddd, Do MMMM, YYYY")
 let time = moment.tz('Asia/Jakarta').format('HH:mm:ss')
-conn.sendHydrated(m.chat, '*『 ʀɪᴍᴜʀᴜ ʙᴏᴛᴢ 』*', `ʜᴀɪ. sᴀʏᴀ ᴀᴅᴀʟᴀʜ ʙᴏᴛ ᴡʜᴀᴛsᴀᴘᴘ, ʙᴏᴛ ɪɴɪ ʙɪsᴀ ᴍᴇᴍʙᴀɴᴛᴜ ᴀɴᴅᴀ ᴜɴᴛᴜᴋ ᴍᴇɴᴅᴏᴡʟᴏᴀᴅ ᴛɪᴋᴛᴏᴋ,ғʙ,ɪɢ,ʏᴏᴜᴛᴜʙᴇ, ᴅᴀɴ ʟᴀɪɴɴʏᴀ. sɪʟᴀʜᴋᴀɴ ᴄʟɪᴄᴋ ʙᴜᴛᴛᴏɴ ᴅɪ ʙᴀᴡᴀʜ,\n\nᴅᴀᴛᴇ: ${date}\nᴛɪᴍᴇ: ${time} ﹙ɢᴍᴛ +7:00)`.trim(), './src/yuki.jpg', 'bit.ly/Papah-Chan', 'ʏᴏᴜᴛᴜʙᴇ ᴏᴡɴᴇʀ', null, null, [
+conn.sendHydrated(m.chat, '*『 ʀɪᴍᴜʀᴜ ʙᴏᴛᴢ 』*', `ʜᴀɪ. sᴀʏᴀ ᴀᴅᴀʟᴀʜ ʙᴏᴛ ᴡʜᴀᴛsᴀᴘᴘ ʏᴀɴɢ ᴅɪᴋᴇᴍʙᴀɴɢᴋᴀɴ ᴏʟᴇʜ ʜᴀɴ, ʙᴏᴛ ɪɴɪ ʙɪsᴀ ᴍᴇᴍʙᴀɴᴛᴜ ᴀɴᴅᴀ ᴜɴᴛᴜᴋ ᴍᴇɴᴅᴏᴡʟᴏᴀᴅ ᴛɪᴋᴛᴏᴋ,ғʙ,ɪɢ,ʏᴏᴜᴛᴜʙᴇ, ᴅᴀɴ ʟᴀɪɴɴʏᴀ. sɪʟᴀʜᴋᴀɴ ᴄʟɪᴄᴋ ʙᴜᴛᴛᴏɴ ᴅɪ ʙᴀᴡᴀʜ,\n\nᴅᴀᴛᴇ: ${date}\nᴛɪᴍᴇ: ${time} ﹙ɢᴍᴛ +7:00)`.trim(), './src/yuki.jpg', 'bit.ly/Papah-Chan', 'ʏᴏᴜᴛᴜʙᴇ ᴏᴡɴᴇʀ', null, null, [
 [`🗡️ᴀʟʟ ᴍᴇɴᴜ`, '.? all'],
 [`🐦ʟɪsᴛ ᴍᴇɴᴜ`, '.menulist']
 [`✨ᴅᴀsʜʙᴏʀᴅ`, '.dashboard']
