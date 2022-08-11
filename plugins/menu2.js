727d7455929072a48fca40d4c31b593c9d2a2ca8
Kaguya Shinomiya <102699105+RyhnXD@users.noreply.github.com>
2022-08-11 07:02:18 +0700

    Update menu2.js

--- plugins/menu2.js
+++ plugins/menu2.js
@@ -1,15 +1,72 @@
-import moment from 'moment-timezone'
-let handler = async (m, { conn, usedPrefix, __dirname, text, command }) => {
-let date = moment.tz('Asia/Jakarta').format("dddd, Do MMMM, YYYY")
-let time = moment.tz('Asia/Jakarta').format('HH:mm:ss')
-conn.sendHydrated(m.chat, '*『 ʀɪᴍᴜʀᴜ ʙᴏᴛᴢ 』*', `ʜᴀɪ. sᴀʏᴀ ᴀᴅᴀʟᴀʜ ʙᴏᴛ ᴡʜᴀᴛsᴀᴘᴘ ʏᴀɴɢ ᴅɪᴋᴇᴍʙᴀɴɢᴋᴀɴ ᴏʟᴇʜ ʜᴀɴ, ʙᴏᴛ ɪɴɪ ʙɪsᴀ ᴍᴇᴍʙᴀɴᴛᴜ ᴀɴᴅᴀ ᴜɴᴛᴜᴋ ᴍᴇɴᴅᴏᴡʟᴏᴀᴅ ᴛɪᴋᴛᴏᴋ,ғʙ,ɪɢ,ʏᴏᴜᴛᴜʙᴇ, ᴅᴀɴ ʟᴀɪɴɴʏᴀ. sɪʟᴀʜᴋᴀɴ ᴄʟɪᴄᴋ ʙᴜᴛᴛᴏɴ ᴅɪ ʙᴀᴡᴀʜ,\n\nᴅᴀᴛᴇ: ${date}\nᴛɪᴍᴇ: ${time} ﹙ɢᴍᴛ +7:00)`.trim(), './src/yuki.jpg', 'bit.ly/Papah-Chan', 'ʏᴏᴜᴛᴜʙᴇ ᴏᴡɴᴇʀ', null, null, [
-[`🗡️ᴀʟʟ ᴍᴇɴᴜ`, '.? all'],
-[`🐦ʟɪsᴛ ᴍᴇɴᴜ`, '.menulist']
-[`✨ᴅᴀsʜʙᴏʀᴅ`, '.dashboard']
-], m, {asLocation: true})
+import fetch from 'node-fetch'
+import { promises, readFileSync } from 'fs'
+import { join } from 'path'
+import { xpRange } from '../lib/levelling.js'
+
+let handler = async(m, { conn, groupMetadata, usedPrefix, text, args, command }) => {
+let imgr = flaaa.getRandom()
+let res = JSON.parse(readFileSync('./json/emoji.json'))
+let em = res.emoji
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
+    let { exp, limit, level, role, money, lastclaim, lastweekly, registered, regTime, age, banned, pasangan } = global.db.data.users[who]
+    let { min, xp, max } = xpRange(level, global.multiplier)
+    let name = await conn.getName(who)
+        if (typeof global.db.data.users[who] == "undefined") {
+      global.db.data.users[who] = {
+        exp: 0,
+        limit: 10,
+        lastclaim: 0,
+        registered: false,
+        name: conn.getName(m.sender),
+        age: -1,
+        regTime: -1,
+        afk: -1,
+        afkReason: '',
+        banned: false,
+        level: 0,
+        lastweekly: 0,
+        role: 'Warrior V',
+        autolevelup: false,
+        money: 0,
+        pasangan: "",
+      }
+     }
+     let math = max - xp
+     let mim_ = ["application/vnd.openxmlformats-officedocument.presentationml.presentation","application/vnd.openxmlformats-officedocument.spreadsheetml.sheet","application/vnd.openxmlformats-officedocument.wordprocessingml.document","text/rtf"]
+     let lin_ = ["https://www.youtube.com","https://www.instagram.com","https://www.facebook.com"]
+     
+  let cap = `*${htki} Information ${htka}*
+
+*☢︎︎ Nama:* *${name}*
+*☢︎︎ Money:* *RP* ${money}
+*☢︎︎ Level* ${level}
+*☢︎︎ Role:* ${role}
+*☢︎︎ XP:* TOTAL ${exp} (${exp - min} / ${xp}) [${math <= 0 ? `Siap untuk *${usedPrefix}levelup*` : `${math} XP lagi untuk levelup`}]
+*☢︎︎ Terdaftar:* ${registered ? 'Ya (' + new Date(regTime).toLocaleString() + ')' : 'Tidak'} ${lastclaim > 0 ? '\n*⏱️Terakhir Klaim:* ' + new Date(lastclaim).toLocaleString() : ''}\n\n Ketik *${usedPrefix}inv* untuk melihat Inventory RPG
+${cmenua}`
+
+let weem = `ɢᴄ ʙᴏᴛ :`
+    await conn.sendButton(m.chat, cap, weem, Buffer.alloc(0), [[em.getRandom() + ' All Menu', usedPrefix + 'allmenu'], [em.getRandom() + ' List Menu', usedPrefix + 'menulist']], m, { mimetype: mim_.getRandom(), fileName: ucapan, pageCount: fpagedoc, fileLength: fsizedoc, seconds: fsizedoc, jpegThumbnail: await( await fetch(thumbnailUrl.getRandom())).buffer(), contextInfo: {
+          externalAdReply :{
+          showAdAttribution: true,
+    mediaUrl: lin_.getRandom(),
+    mediaType: 2,
+    description: wm, 
+    title: '👋 Hai, ' + name,
+    body: botdate,
+    thumbnail: await( await fetch(pp)).buffer(),
+    sourceUrl: sgc
+     }}
+  })
 }
-handler.help = ['menu']
+
+handler.help = ['menu', 'help', '?']
 handler.tags = ['main']
-handler.command = /^(menu)$/i
+handler.command = /^(menu|help|\?)$/i
 
 export default handler
+
+function pickRandom(list) {
+     return list[Math.floor(Math.random() * list.length)]
+  }
